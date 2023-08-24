2927888adfe456a1e71fb94643018c88c30ea6b2
Gabzz <sidney.appiah@gmail.com>
2023-08-24 10:32:13 +0000

    Created sign-up form using installed ejs plugin and styled it using skeleton cdn

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ const fileRoutes = require('./src/routes');
 const app = express();
 const port = process.env.port || 3000;
 
+app.set('view engine', 'ejs')
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
+
+app.get('/users/register', (req, res) => {
+    res.render('register');
+})
+
+app.get('/users/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/users/dashboard', (req, res) => {
+    res.render('dashboard', {user: "Gabzz"});
+})
+
+
 
 app.use('/api/v1/FileServer', fileRoutes);
 
